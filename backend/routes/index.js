1002e153cec6ec02eeb777da928e944b1ecafa94
luca-mocini <lucamocini12@gmail.com>
2024-11-21 11:29:36 +0100

    enabled middleware and test controllers

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const authController = require('../controllers/authController');
 const testController = require('../controllers/testController');
-//const authMiddleware = require('../middlewares/authMiddleware');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 console.log('Auth Controller:', authController);
 console.log('Test Controller:', testController);
@@ -13,11 +13,11 @@ router.post('/login', authController.login);
 router.post('/register', authController.register); 
 
 // Test Management (Protected)
-//router.post('/tests', authMiddleware, testController.createTest);
-//router.get('/tests/:tenantId', authMiddleware, testController.getTests);
-//router.get('/test/:id', authMiddleware, testController.getTestById);
-//router.put('/test/:id', authMiddleware, testController.updateTest);
-//router.delete('/test/:id', authMiddleware, testController.deleteTest);
+router.post('/tests', authMiddleware, testController.createTest);
+router.get('/tests/:tenantId', authMiddleware, testController.getTests);
+router.get('/test/:id', authMiddleware, testController.getTestById);
+router.put('/test/:id', authMiddleware, testController.updateTest);
+router.delete('/test/:id', authMiddleware, testController.deleteTest);
 
 module.exports = router;
 
