e1fea45203433baab7825acf9b5343bb1474463d
luca-mocini <lucamocini12@gmail.com>
2024-11-21 14:04:04 +0100

    x

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,21 +1,21 @@
 const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
-  const token = req.header('Authorization')?.replace('Bearer ', '');  // Estrarre il token dal header
-
-  if (!token) {
-    return res.status(401).json({ message: 'Authentication required' });
-  }
-
-  try {
-    // Decodifica il token
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;  // Aggiungi i dati decodificati all'oggetto request
-    next();  // Passa al prossimo middleware/route handler
-  } catch (error) {
-    res.status(401).json({ message: 'Invalid token' });
-  }
-};
+    const token = req.header('Authorization')?.replace('Bearer ', '');  // Estrarre il token dal header
+  
+    if (!token) {
+      return res.status(401).json({ message: 'Authentication required' });
+    }
+  
+    try {
+      // Decodifica il token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded;  // Aggiungi i dati decodificati all'oggetto request
+      next();  // Passa al prossimo middleware/route handler
+    } catch (error) {
+      res.status(401).json({ message: 'Invalid token' });
+    }
+  };
 
 module.exports = authMiddleware;
 
