e3271a03bdadc29589db9918a1d5a07ce4c3131b
luca-mocini <lucamocini12@gmail.com>
2024-11-22 11:58:39 +0100

    ss

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';  // Importa il componente principale
-import './index.css';  // Importa il file CSS di base
+import './css/styles.css';  // Importa il file CSS di base
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
