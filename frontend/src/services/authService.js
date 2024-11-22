cc298bc320ab62cffca445057fdabc7d71a38493
luca-mocini <lucamocini12@gmail.com>
2024-11-22 12:43:22 +0100

    ss

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,17 +1,17 @@
-export const login = async (email, password) => {
+export const login = async (username, password) => {
     const response = await fetch(process.env.REACT_APP_BACKEND_URL + '/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ email, password }),
+      body: JSON.stringify({ username, password }),
     });
     return response.json();
   };
   
-  export const register = async (email, password) => {
+  export const register = async (username, password) => {
     const response = await fetch(process.env.REACT_APP_BACKEND_URL + '/register', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ email, password }),
+      body: JSON.stringify({ username, password }),
     });
     return response.json();
   };
