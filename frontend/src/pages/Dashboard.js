baf6cf90c82c3c384bf330ea90c68dfe5e8bedea
luca-mocini <lucamocini12@gmail.com>
2024-11-22 13:11:04 +0100

    ss

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,14 +1,19 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
+import { Navigate } from 'react-router-dom';
 
 const Dashboard = () => {
   const { user } = useContext(AuthContext);
 
+  if (!user) {
+    return <Navigate to="/" />;  // Reindirizza alla login se l'utente non è autenticato
+  }
+
   return (
     <div>
       <h1>Welcome to your Dashboard</h1>
-      <p>usename:: {user?.username}</p>
-      <p>Tenant: {user?.tenant}</p>
+      <p>Username: {user.username}</p>
+      <p>Tenant: {user.tenant || 'No tenant assigned'}</p>
     </div>
   );
 };
