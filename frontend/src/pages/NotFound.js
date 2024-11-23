0e5f139dec98847eb573d7b4ff354250e70141d5
luca-mocini <lucamocini12@gmail.com>
2024-11-23 15:21:26 +0100

    Added custom style to NotFound page custom css

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import '../styles/NotFound.css'; // Importa il CSS specifico per NotFound
+
 
 const NotFound = () => {
   return (
-    <div>
+    <div className="notfound-container">
       <h1>404 - Page Not Found</h1>
       <p>The page you are looking for does not exist.</p>
+      <a href="/">Go to Homepage</a> {/* Link per tornare alla homepage */}
     </div>
   );
 };
