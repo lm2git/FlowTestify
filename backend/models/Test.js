6dae1ca93fee7710e614be48023ed55aacdbd1cb
luca-mocini <lucamocini12@gmail.com>
2024-11-23 13:18:36 +0100

    ss

--- backend/models/Test.js
+++ backend/models/Test.js
@@ -4,8 +4,8 @@ const Step = require('./Step');  // Importa il modello di Step
 // Schema del Test
 const testSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  tenantName: { type: String, required: true }, // Aggiungi tenantName
-  steps: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Step' }]  // Fai riferimento al modello Step
+  steps: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Step' }],  // Fai riferimento al modello Step
+  tenantName: { type: String, required: true } // Aggiungi tenantName
 });
 
 module.exports = mongoose.model('Test', testSchema);
