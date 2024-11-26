5a219dba367103e9d2e870afac891ffd504b4a6d
luca-mocini <lucamocini12@gmail.com>
2024-11-26 15:49:09 +0100

    ss

--- backend/models/Test.js
+++ backend/models/Test.js
@@ -1,12 +1,6 @@
 const mongoose = require('mongoose');
+const Step = require('../models/Step');
 
-const stepSchema = new mongoose.Schema({
-  description: String, // Descrizione dello step
-  actionType: String,  // Tipo di azione (es. "click", "go to url", etc.)
-  value: String,       // Valore associato all'azione (es. URL per "go to url")
-  status: String,      // Stato del test step (es. "success", "failed", etc.)
-  order: Number,       // Ordine dello step
-});
 
 // Schema del Test
 const testSchema = new mongoose.Schema({
