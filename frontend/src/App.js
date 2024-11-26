07cb3d924f0bd83aa5f18010f0432106966f3e2f
luca-mocini <lucamocini12@gmail.com>
2024-11-26 13:39:15 +0100

    rimossa future={{ v7_startTransition: true, v7_relativeSplatPath: true }} dal JSX e aggiungi la configurazione corretta per il RouterProvider.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,45 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';  // Importa il contesto
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import NotFound from './pages/NotFound';
 
+
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+    {
+      path: "/dashboard",
+      element: <Dashboard />,
+    },
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+  ],
+  {
+    future: {
+      v7_startTransition: true,
+      v7_relativeSplatPath: true,
+    },
+  }
+);
+
 const App = () => {
   return (
     <AuthProvider>
-      <Router>
-        future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
+      <RouterProvider router={router} />
     </AuthProvider>
   );
 };
